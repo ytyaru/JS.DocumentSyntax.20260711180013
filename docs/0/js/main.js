de83f98e7b02cfb17647ffd48bd0c5eb5608f668
ytyaru <yttry0@gmail.com>
2026-07-11 19:27:20 +0900

    Created a repository

---
+++ docs/0/js/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+    const author = 'ytyaru';
+    van.add(document.querySelector('main'), 
+        van.tags.h1(van.tags.a({href:`https://github.com/${author}/JS.DocumentSyntax.20260711180013/`}, 'DocumentSyntax')),
+        van.tags.p('人間可読なテキストである文書の構文を規格するための体系。'),
+//        van.tags.p('A system for standardizing the syntax of human-readable text documents.'),
+    );
+    van.add(document.querySelector('footer'),  new Footer('ytyaru', '../').make());
+
+    const a = new Assertion();
+    a.t(true);
+    a.f(false);
+    a.e(TypeError, `msg`, ()=>{throw new TypeError(`msg`)});
+    a.fin();
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
